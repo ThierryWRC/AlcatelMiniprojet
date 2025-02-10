eac6e542eb5fd7074cdef9bb90623575bc9c8a5f
ThierryWRC <thierry_boissard@yahoo.com>
2025-02-10 17:31:00 +0100

    Passer en paramètre URL Serveur

--- apps/miniprojet/src/app/services/api/api.service.ts
+++ apps/miniprojet/src/app/services/api/api.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Item } from '@miniprojet/models';
 import { catchError, map, Observable } from 'rxjs';
+import { environment } from './../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
   private readonly _http: HttpClient = inject(HttpClient);
 
   sendToBackend$(items: Item[]): Observable<string> {
-    return this._http.post('http://localhost:3000/kraken', items).pipe(
+    return this._http.post(environment.serverUrl + '/kraken', items).pipe(
       map((response: any) => {
         debugger;
         return response.message;
