d1ac6b5c28b51d9c08b4c69dfcb130376f77d9f9
ThierryWRC <thierry_boissard@yahoo.com>
2025-02-10 16:17:26 +0100

    corrections tests

--- apps/miniprojet/src/app/loader/loader.component.spec.ts
+++ apps/miniprojet/src/app/loader/loader.component.spec.ts
@@ -1,10 +1,19 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoaderComponent } from './loader.component';
+import { LoaderService } from './loader.service';
 
 describe('LoaderComponent', () => {
   let component: LoaderComponent;
+  let fixture: ComponentFixture<LoaderComponent>;
 
-  beforeEach(() => {
-    component = new LoaderComponent();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [LoaderService],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
--- libs/utils/src/lib/utils.spec.ts
+++ libs/utils/src/lib/utils.spec.ts
@@ -1,1 +1,13 @@
+import { Utils } from './utils';
 
+describe('Utils', () => {
+  let component: Utils;
+
+  beforeEach(() => {
+    component = new Utils();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
